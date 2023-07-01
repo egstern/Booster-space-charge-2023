a6fd7fd5a4f99216a253d0cc0bf88a915cccd429
Eric G. Stern <egstern@fnal.gov>
2023-06-30 21:36:05 -0500

    file reading the lattice

--- working/booster_lattice.py
+++ working/booster_lattice.py
@@ -34,9 +34,37 @@ def get_booster_lattice():
 
     lattice.set_reference_particle(refpart)
 
+    a = synergia.simulation.Lattice_simulator.tune_circular_lattice(lattice)
+    print('tcl: ', a)
+    print('closed orbit length: ', a[4]*refpart.get_beta())
     return lattice
 
 if __name__ == "__main__":
     lattice = get_booster_lattice()
 
     print('Read lattice, length: ', lattice.get_length(), ", number elements: ", len(lattice.get_elements()))
+
+    refpart = lattice.get_reference_particle()
+    etot = refpart.get_total_energy()
+    pmom = refpart.get_momentum()
+    gamma = refpart.get_gamma()
+    beta = refpart.get_beta()
+    print('Lattice energy: ', etot)
+    print('Lattice momentum: ', pmom)
+    print('Lattice gamma: ', gamma)
+    print('Lattice beta: ', beta)
+
+    print('RF cavities:')
+    for elem in lattice.get_elements():
+        if elem.get_type() == synergia.lattice.element_type.rfcavity:
+            print(elem)
+
+    L = lattice.get_length()
+    c = synergia.foundation.pconstants.c
+    h = 84
+    f = h * beta * c/L
+    print('frequency should be: ', f)
+
+    closed_orbit = synergia.simulation.Lattice_simulator.calculate_closed_orbit(lattice)
+    print('closed orbit: ', closed_orbit)
+    
